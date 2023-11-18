5ad3e2307a4b02480a87c603a507d0c60b1f99b6
joaofradept <131976573+joaofradept@users.noreply.github.com>
2023-11-18 13:43:41 +0000

    Player movement + environment & character assets
    Player movement includes script and animations

---
+++ Interview Task @ Blue Gravity Studios/Assets/_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    Animator anim;
+
+    Vector2 movementInput;
+    Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Get movement from input
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        if (movementInput.x != 0)
+        {
+            movement.x = movementInput.x;
+            movement.y = 0;
+        }
+        if (movementInput.y != 0)
+        {
+            movement.y = movementInput.y;
+            movement.x = 0;
+        }
+        if (movementInput.sqrMagnitude <= 0.01f)
+        {
+            movement.x = 0;
+            movement.y = 0;
+        }
+
+        // If there's no movement at all, don't send it to animator
+        // so that animator keeps info about the last direction
+        // and uses it to set the idle animation
+        if (movement.x != 0 || movement.y != 0)
+        {
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+        }
+
+        // Set movement speed in animator
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply movement to rigidbody
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
